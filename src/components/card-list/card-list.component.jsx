fd0e2fd0d17019803119ce06a85ed5b09ebb802a
rtaraboo <rtarabocchia@gmail.com>
2022-07-14 18:25:27 -0400

    Coverting componets to func.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,26 @@
-import { Component } from "react";
+// import { Component } from "react";
 import MonsterCard from "../monster-card/monster-card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <MonsterCard monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <MonsterCard monster={monster} />;
+    })}
+  </div>
+);
+
+// class CardList extends Component {
+//   render() {
+//     const { monsters } = this.props;
+//     return (
+//       <div className="card-list">
+//         {monsters.map((monster) => {
+//           return <MonsterCard monster={monster} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default CardList;
--- src/components/monster-card/monster-card.component.jsx
+++ src/components/monster-card/monster-card.component.jsx
@@ -1,20 +1,34 @@
-import { Component } from "react";
-import "./monster-card.styles.css"
+// import { Component } from "react";
+import "./monster-card.styles.css";
 
-class MonsterCard extends Component {
-  render() {
-    const { name, email, id } = this.props.monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt="monster"
-          src={`https://www.robohash.org/${id}?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
+const MonsterCard = ({ monster }) => {
+  const { name, email, id } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt="monster"
+        src={`https://www.robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
 }
+
+// class MonsterCard extends Component {
+//   render() {
+//     const { name, email, id } = this.props.monster;
+//     return (
+//       <div className="card-container" key={id}>
+//         <img
+//           alt="monster"
+//           src={`https://www.robohash.org/${id}?set=set2&size=180x180`}
+//         />
+//         <h2>{name}</h2>
+//         <p>{email}</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default MonsterCard;
